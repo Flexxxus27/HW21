a8e7008ffa60dc4e4c30d555f90ad3b4a7120e08
Алексей <berezin2001@bk.ru>
2024-01-20 21:31:06 +0300

    Add project files.

---
+++ HW21/Main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+
+int main() {
+	setlocale(0, "");
+	int x, y;
+
+	//Задание 1.
+
+	std::cout << "Задание 1.\n\n";
+	std::cout << "Введите значение X -> ";
+	std::cin >> x;
+	std::cout << "Введите значение Y -> ";
+	std::cin >> y;
+	int* px = &x;
+	int* py = &y;
+	
+	int temp = *px;
+	*px = *py;
+	*py = temp;
+	std::cout << "X = " << x << std::endl;
+	std::cout << "Y = " << y << std::endl;
+
+
+
+
+	return 0;
+}
